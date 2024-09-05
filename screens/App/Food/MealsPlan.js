7640e6b93f1e43ff1c54ee25dba164e9ec0c4579
GrxnGee <sunya070609@gmail.com>
2024-09-05 20:46:57 +0700

    all about mealplan

--- screens/App/Food/MealsPlan.js
+++ screens/App/Food/MealsPlan.js
@@ -1,66 +1,144 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, ScrollView, Image } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, ScrollView, Image, Alert } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { db, auth } from '../../../Firebase';
 import { Ionicons } from '@expo/vector-icons';
-import { doc, onSnapshot } from 'firebase/firestore';
+import { doc, onSnapshot, updateDoc, arrayUnion, deleteDoc, getDoc } from 'firebase/firestore';
 
 export default function MealsPlan({ navigation }) {
     const user = auth.currentUser;
     const [mealPlan, setMealPlan] = useState([]);
     const [totalCalories, setTotalCalories] = useState(0);
+    const [userInfo, setUserInfo] = useState(null);
 
-    useEffect(() => {
-        if (user) {
-            console.log('Current User ID:', user.uid);
-            const unsubscribe = onSnapshot(doc(db, 'mealPlans', user.uid), (doc) => {
-                if (doc.exists()) {
-                    const mealPlanData = doc.data();
-                    console.log('Real-time meal plan data:', mealPlanData);
-                    setMealPlan(mealPlanData.foodItems || []);
-                    const total = (mealPlanData.foodItems || []).reduce((sum, item) => sum + (item.cal || 0), 0);
-                    setTotalCalories(total);
-                } else {
-                    console.log('No meal plan found for user:', user.uid);
-                    setMealPlan([]);
-                    setTotalCalories(0);
-                }
-            });
-            return () => unsubscribe();
-        } else {
-            console.log('No user');
-        }
-    }, [user]);
 
-    const UpdateUserMeal = async (food) => {
-        const userMealPlanDoc = doc(db, 'users', user.uid);
-
-        try {
-            const docSnapshot = await getDoc(userMealPlanDoc);
-
-            if (docSnapshot.exists()) {
-                await updateDoc(userMealPlanDoc, {
-                    foodItems: arrayUnion(food)
-                });
+   useEffect(() => {
+    if (user) {
+        const userDocRef = doc(db, "users", user.uid);
+        const unsubscribe = onSnapshot(userDocRef, (doc) => {
+            if (doc.exists()) {
+                setUserInfo(doc.data()); 
             } else {
-                await setDoc(userMealPlanDoc, {
-                    foodItems: [food]
-                });
+                Alert.alert('Error', 'No such document!');
             }
+        }, (error) => {
+            console.error("Error fetching user data: ", error);
+            Alert.alert('Error', 'Failed to fetch user data.');
+        });
 
+        return () => unsubscribe();
+    }
+}, [user]);
+
+
+useEffect(() => {
+    if (user) {
+        const unsubscribe = onSnapshot(doc(db, 'mealPlans', user.uid), (doc) => {
+            if (doc.exists()) {
+                const mealPlanData = doc.data();
+                setMealPlan(mealPlanData.foodItems || []);
+                const total = (mealPlanData.foodItems || []).reduce((sum, item) => sum + (item.cal || 0), 0);
+                setTotalCalories(total);
+            } else {
+                setMealPlan([]);
+                setTotalCalories(0);
+            }
+        });
+        return () => unsubscribe();
+    }
+}, [user]);
+
+const UpdateUserMeal = async (food) => {
+    const userMealPlanDoc = doc(db, 'users', user.uid);
+
+    try {
+        const docSnapshot = await getDoc(userMealPlanDoc);
+        const currentDate = new Date().toISOString().split('T')[0];
+
+        if (docSnapshot.exists()) {
+            const userData = docSnapshot.data();
+            const lastUpdated = userData.lastUpdated || '';
+            const userTDEE = userInfo?.tdee || 0;
+
+      
+            if (totalCalories > userTDEE / 3) {
+                Alert.alert('Warning', 'Total calories exceed one-third of your daily recommended intake.');
+                return;
+            } else {
+
+                if (lastUpdated === currentDate) {
+                    const currentFoodValue = userData.food || 0;
+                    await updateDoc(userMealPlanDoc, {
+                        food: currentFoodValue + totalCalories,
+                        lastUpdated: currentDate,
+                        tdee: userData.tdee - totalCalories
+                    });
+                } else {
+                    await updateDoc(userMealPlanDoc, {
+                        food: totalCalories,
+                        lastUpdated: currentDate,
+                        tdee: userData.tdee - totalCalories
+                    });
+                }
+                setTimeout(async () => {
+                    await DeleteCurrentMealPlan();
+                    navigation.goBack();
+                }, 2000);
+                alert('Added to your meal plan!');
+            }
+        } else {
+            await setDoc(userMealPlanDoc, {
+                food: totalCalories,
+                lastUpdated: currentDate
+            });
             alert('Added to your meal plan!');
-        } catch (error) {
-            console.error("Error updating meal plan: ", error);
-            alert('Failed to add food item to your meal plan.');
         }
-    };
+    } catch (error) {
+        console.error("Error updating meal plan: ", error);
+        alert('Failed to add food item to your meal plan.');
+    }
+};
 
-    const PressSaveMealCal = async (food) => {
-        await UpdateMeal(food);
-        setTimeout(() => {
-            goBack();
-        }, 2000);
-    };
+
+const deleteFoodItem = async (foodItemId) => {
+    const mealPlanDoc = doc(db, 'mealPlans', user.uid);
+
+    try {
+        const docSnapshot = await getDoc(mealPlanDoc);
+        if (docSnapshot.exists()) {
+            const mealPlanData = docSnapshot.data();
+            const updatedFoodItems = mealPlanData.foodItems.filter(item => item.id !== foodItemId);
+            await updateDoc(mealPlanDoc, {
+                foodItems: updatedFoodItems
+            });
+
+            setMealPlan(updatedFoodItems);
+            const total = updatedFoodItems.reduce((sum, item) => sum + (item.cal || 0), 0);
+            setTotalCalories(total);
+        }
+    } catch (error) {
+        console.error("Error deleting food item: ", error);
+        alert('Failed to delete food item.');
+    }
+};
+
+const PressSaveMealCal = async (food) => {
+    await UpdateUserMeal(food);
+};
+
+const DeleteCurrentMealPlan = async () => {
+    const mealPlanDoc = doc(db, 'mealPlans', user.uid);
+
+    try {
+        const docSnapshot = await getDoc(mealPlanDoc);
+        if (docSnapshot.exists()) {
+            await deleteDoc(mealPlanDoc);
+        }
+    } catch (error) {
+        console.error("Error deleting meal plan: ", error);
+        alert('Failed to delete current meal plan.');
+    }
+};
 
     console.log('Meal plan after setting:', mealPlan);
     console.log('Total calories:', totalCalories);
@@ -90,6 +168,9 @@ export default function MealsPlan({ navigation }) {
                                     <Text style={styles.cardText}>{item.fname}</Text>
                                 </View>
                                 <Text style={styles.calText}>{item.cal} Calories</Text>
+                                <TouchableOpacity onPress={() => deleteFoodItem(item.id)} style={styles.deleteButton}>
+                                    <Text style={styles.deleteButtonText}>Delete</Text>
+                                </TouchableOpacity>
                             </View>
                         </View>
                     ))
@@ -163,75 +244,70 @@ const styles = StyleSheet.create({
     },
     cardContainer: {
         alignItems: 'center',
-        marginBottom: 20,
+        marginBottom: 15,
     },
     Foodcard: {
-        backgroundColor: '#FFFFFF',
-        padding: 10,
-        borderRadius: 15,
-        width: 350,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 15,
+        width: '90%',
     },
     topRow: {
         flexDirection: 'row',
         alignItems: 'center',
-        marginBottom: 5,
+        marginBottom: 10,
     },
     cardImage: {
-        width: 100,
-        height: 100,
+        width: 70,
+        height: 70,
         borderRadius: 10,
         marginRight: 10,
     },
     cardText: {
         fontSize: 18,
-        color: '#000',
-        fontWeight: 'bold'
+        fontWeight: 'bold',
     },
     calText: {
         fontSize: 16,
-        color: '#777',
-        marginLeft: '40%',
-    },
-    noMealsText: {
-        textAlign: 'center',
-        marginTop: 20,
-        fontSize: 18,
         color: 'gray',
+    },
+    deleteButton: {
+        backgroundColor: '#FF4C4C',
+        borderRadius: 5,
+        padding: 5,
+        alignItems: 'center',
+        marginTop: 10,
+        width: '30%',
+        alignSelf: 'center',
+    },
+    deleteButtonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
     },
     calculateCal: {
         backgroundColor: 'white',
-        width: 300,
-        borderRadius: 15,
-        flexDirection: 'column',
+        padding: 10,
+        borderRadius: 10,
         alignItems: 'center',
-        marginLeft: '10%',
-    },
-    CalRow: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingLeft: 5,
-        paddingRight: 5
-    },
-    CalcardContainer: {
-        width: '100%',
-        marginBottom: 5,
-        alignItems: 'flex-start',
-    },
-    itemName: {
-        marginRight: 15,
-        fontWeight: 'bold',
-        color: '#4A9B5D',
-    },
-    itemCal: {
-        marginRight: 15,
-    },
-    itemCalories: {
-        fontWeight: 'bold',
     },
     totalCaloriesText: {
-        marginTop: 10,
-        fontWeight: 'bold',
         fontSize: 18,
-        textAlign: 'center'
+        fontWeight: 'bold',
+        marginTop: 15,
     },
+    button: {
+        backgroundColor: '#4A9B5D',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    CalRow:{
+        justifyContent:'center',
+        alignContent:'center'
+    },
+    CalcardContainer : {
+         justifyContent:'center',
+        alignContent:'center'
+    }
 });
